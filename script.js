84def5057b5293d78d51666cb43e2449d81ad480
Sanjana G Patil <sanjanagpatil.10@gmail.com>
2025-07-24 00:02:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Scroll effect for navbar
+window.addEventListener("scroll", () => {
+  const nav = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
+
+
+
+ 
